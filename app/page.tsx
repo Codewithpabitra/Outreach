93e3904fa45f8f87fa689fd6fa45e99d7a667595
Pabitra Maity <pabitra05.maity@gmail.com>
2026-08-29 16:10:05 +0530

    test: verify Qodo review trigger

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
       Hello world
 
       <button  className="text-white px-5 py-2 bg-zinc-800 rounded-lg m-2  cursor-pointer">click me </button>
+
+      <button className="px-2 py-2 bg-blue-500 text-white text-lg m-2">Check button</button>
     </div>
   )
 }
