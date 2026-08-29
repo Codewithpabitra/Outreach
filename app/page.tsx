ef4d67fd287eb5e19dfd8a16826741fd15397d75
Pabitra Maity <pabitra05.maity@gmail.com>
2026-08-29 16:05:06 +0530

    init: initial nextjs scaffold

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="text-xl text-red-500">
+      Hello world
+
+      <button  className="text-white px-5 py-2 bg-zinc-800 rounded-lg m-2  cursor-pointer">click me </button>
+    </div>
+  )
+}
